6d1cde64b152629f49e712946f22069663e03a1b
Mark Matthews <tronious@gmail.com>
2026-01-06 13:08:00 -0500

    Update port for Azure deployment

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // middleware
 app.use(cors());
